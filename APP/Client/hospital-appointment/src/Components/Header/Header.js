1112526837c75c7b2dab94d83636efc644f753fc
mmalla <manisha.malla@gmail.com>
2021-11-29 12:12:38 +0400

    Update Header.js

--- APP/Client/hospital-appointment/src/Components/Header/Header.js
+++ APP/Client/hospital-appointment/src/Components/Header/Header.js
@@ -7,8 +7,6 @@ function Header() {
   console.error("stream deleted");
   console.error("stream deleted for hospital-demo-server.herokuapp.com/appointments/patient/sloits/<id>");
   console.info("RtmInvalidStatusError:: Informational Log");
-  const myError = new Error("please improve your code");
-  throw myError;
   return (
     <HeaderCont>
       <Hospital>
