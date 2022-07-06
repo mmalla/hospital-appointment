93e7158339a6bda90f4675fc24bef1210587ccc8
mmalla <manisha.malla@gmail.com>
2022-07-06 14:55:32 +0400

    Update userActions.js

--- APP/Client/hospital-appointment/src/redux/actions/userActions.js
+++ APP/Client/hospital-appointment/src/redux/actions/userActions.js
@@ -5,7 +5,7 @@ export const Login = (credentials) => (dispatch) => {
   return new Promise((resolve, reject) => {
     patientLogin(credentials)
       .then((res) => {
-      window.zipy.identify("test_enduserinfo", {
+      window.zipy.identify(1234, {
         customerName: "Pixelphone Inc.",
         phone: "202-555-0112",
         firstName: "Jack",
