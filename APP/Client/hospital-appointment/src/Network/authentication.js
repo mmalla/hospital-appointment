6dc73d3411f004231f5d79e843f38c6f30bce1bc
mmalla <manisha.malla@gmail.com>
2022-12-15 13:29:44 +0400

    Update authentication.js

--- APP/Client/hospital-appointment/src/Network/authentication.js
+++ APP/Client/hospital-appointment/src/Network/authentication.js
@@ -2,6 +2,27 @@ import axios from "axios";
 import { SERVER_URL } from "../constants";
 
 export const patientLogin = (credentials) => {
+  let user = {
+      "name": "Pragati",
+      "age": "23"
+  }
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type':
+          'application/json;charset=utf-8',
+        'Authorization': 'qabcdefghijkl1232344'
+      },
+      body: JSON.stringify(user)
+    }
+    // Fake api for making post requests
+    let fetchRes = fetch(
+      "http://dummy.restapiexample.com/api/v1/create",
+      options);
+    fetchRes.then(res =>
+      res.json()).then(d => {
+        console.log("hello----------------");
+      });
   return new Promise((resolve, reject) => {
     axios
       .post(SERVER_URL + "/authentication/login/patient", credentials)
